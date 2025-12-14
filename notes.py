9daba5edf80c0576c401ad302587d320175163a9
Meghansh Maurya <77629912+Meghansh-Maurya@users.noreply.github.com>
2025-12-14 17:38:10 +0530

    Day 4: Added server-generated IDs and separated input/output models

--- notes.py
+++ notes.py
@@ -4,41 +4,48 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class Note(BaseModel):
+class NoteClient(BaseModel):
     title: str
     content: str
 
+class NoteServer(BaseModel):
+    id : int
+    title : str
+    content : str
 
 notes = {}
 
+note_id = 0
 
-@app.get('/notes/{note_id}')
+
+@app.get('/notes/{note_id}', response_model=NoteServer)
 def get_notes(note_id: int):
     if note_id not in notes:
         raise HTTPException(status_code=404, detail="Note not found")
-    return {"note_id": note_id,
-            "note": notes[note_id]}
+    return notes[note_id]
 
 
-@app.post('/notes/{note_id}')
-def write_note(note: Note, note_id: int):
+@app.post('/notes/', response_model=NoteServer)
+def write_note(note: NoteClient):
+    global note_id
+    note_id += 1
 
-    if note_id in notes:
-        raise HTTPException(status_code=400, detail="Note already exists")
+    notes[note_id] = note.model_dump() #model_dump() converts pydantic model to dictionary
+    notes[note_id]['id'] = note_id #adding id to the note dictionary
 
-    notes[note_id] = note.model_dump()
-    return {"message": "Successfully Added",
-            "note_id": note_id,
-            "note": notes[note_id]}
+    # note_data = note.model_dump()
+    # note_data["id"] = note_id
+    # notes[note_id] = note_data
+    
+    return notes[note_id]
 
 
-@app.put('/notes/{note_id}')
-def update_note(note_id: int, note: Note):
+@app.put('/notes/{note_id}', response_model=NoteServer)
+def update_note(note_id: int, note: NoteClient):
     if note_id in notes:
         notes[note_id] = note.model_dump()
-        return {"message": "Successfully Updated",
-                "note_id": note_id,
-                "note": notes[note_id]}
+        notes[note_id]['id'] = note_id
+        return notes[note_id]
 
     else:
         raise HTTPException(status_code=404, detail="Note not found")
