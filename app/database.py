5dca4e7042f6c48318d91a6103df923fef6bc5d5
Meghansh Maurya <meghansh.maurya@gmail.com>
2025-12-22 01:45:52 +0530

    Added CORS and switch to PostgreSQL and prepare for deployment

--- app/database.py
+++ app/database.py
@@ -1,12 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL,
-                       connect_args={"check_same_thread": False}
-                       )
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import router
 from app.database import engine
 from app.models import Base
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8501",
+        "http://127.0.0.1:8501"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 Base.metadata.create_all(bind=engine)
